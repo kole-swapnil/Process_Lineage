f12030284ec8edf7dae490fd564fbc6528ce1a06
Swapnil Kole <swapnilkole7500@gmail.com>
2020-11-14 18:09:52 +0530

    boilerplate setup for react

---
+++ client/src/Components/MainComponents.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import Contract from "../contracts/SimpleStorage.json";
+import getWeb3 from "../getWeb3";
+import "../App.css";
+import Header from "./HeaderComponent";
+
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
+import Footer from './FooterComponent';
+
+
+//import HDWalletProvider from "@truffle/hdwallet-provider";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { storageValue: 0, web3: null, accounts: null, contract: null ,res : null,auth:false};
+
+  }
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = Contract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        Contract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+     
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      console.log(instance)
+      this.setState({ web3, accounts, contract: instance });
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+
+
+  render() {
+    
+    return (
+      <div className="App">
+        <Header />
+
+      
+        <Footer/>
+      </div>
+    )
+
+
+
+  }
+}
+
+export default Main;
