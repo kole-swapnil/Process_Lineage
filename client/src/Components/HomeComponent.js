176bed59014da71e796bbd4b7a63bbaef34f3922
Pallavi Mishra <pallavimishra.paradip@gmail.com>
2020-11-17 14:17:51 +0530

    home

--- client/src/Components/HomeComponent.js
+++ client/src/Components/HomeComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem } from 'reactstrap';
 import { BrowserRouter, NavLink } from 'react-router-dom';
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
 import '../App.css'
+import { Container, Row, Col,Card,CardImg} from 'reactstrap';
+
+
 
 class Home extends Component{
     constructor(props){
@@ -15,26 +16,35 @@ class Home extends Component{
       
         return(
             <React.Fragment>
+                 <div className="container">
                 <div className="inner header">
                     <h6>SignIn to grab exciting offers and in-time deliveries</h6>
-                      <div className="img-1">
-                        <img src="https://cdn.pocket-lint.com/r/s/970x/assets/images/141307-laptops-review-microsoft-surface-laptop-review-image1-lzayz9jcmq.jpg"></img>
-                      </div>
-                      <div className="img-2">
-                        <img src="https://www.cined.com/content/uploads/2020/05/Dell-XPS-15-17-featured.jpg"></img>
-                      </div>
-                        <div className="img-3">
-                        <img src="https://d2pa5gi5n2e1an.cloudfront.net/ph/images/article/5170/img-1.jpg"></img>
-                        </div>
-                        <div className="img-4">
-                            <img src="https://tse1.mm.bing.net/th?id=OIP.DYmDXHlvuoA43eCoX7ZPYQHaE8&pid=Api&P=0&w=271&h=181"></img>
-                        </div>
+                <Container>
+                <Card style={{marginTop: 30, marginBottom:40}}>
+                  <CardImg top width="80%" src="https://www.cined.com/content/uploads/2020/05/Dell-XPS-15-17-featured.jpg"/>
+                </Card>
+                <Card style={{marginTop: 30}}>
+                  <CardImg top width="80%" src="https://cdn.pocket-lint.com/r/s/970x/assets/images/124434-phones-buyer-s-guide-best-android-phones-2017-these-are-the-androids-you-re-looking-for-image1-kiqmufsv8x.jpg" />
+                </Card>
+                <Card style={{marginTop: 30 }}>
+                  <CardImg top width="80%" src="https://www.cgcomputers.co.uk/data/files/lenovo.jpg" />
+                </Card>
+                <Card style={{marginTop: 30 }}>
+                  <CardImg top width="80%" src="https://i2.wp.com/www.smartprix.com/bytes/wp-content/uploads/2020/06/Mi-laptop.jpg?fit=1200%2C675&ssl=1"/>
+                </Card>
+                <Card style={{marginTop: 30}}>
+                  <CardImg top width="80%" src="https://images.anandtech.com/doci/11455/dell_inspiron_7000_1.jpg"/>
+                </Card>
+                <Card style={{marginTop: 30}}>
+                  <CardImg top width="80%" src="http://static4.businessinsider.com/image/598a08ec76084a2b198b513a-1190-625/this-eye-catching-new-phone-out-of-china-is-a-great-example-of-where-the-future-of-smartphones-is-headed.jpg"/>
+                </Card>
+                </Container>
+                    </div></div>
 
-                    </div>
-                
-                
-            
-            </React.Fragment>
+
+                </React.Fragment>
+
+               
         )
 
     }
