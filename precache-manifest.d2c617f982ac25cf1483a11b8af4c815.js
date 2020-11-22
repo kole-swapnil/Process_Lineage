2d56fe442b42846a9e6c0b5fbe1f5e09182c62a0
Swapnil Kole <swapnilkole7500@gmail.com>
2020-11-22 19:04:32 +0530

    Updates

---
+++ precache-manifest.d2c617f982ac25cf1483a11b8af4c815.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f76b4f9c6f13ef62d9418a026bfbc50",
+    "url": "/Process_Lineage/index.html"
+  },
+  {
+    "revision": "5c7a8add3fbec3be4262",
+    "url": "/Process_Lineage/static/css/2.5854d024.chunk.css"
+  },
+  {
+    "revision": "852436e734a80e0a887f",
+    "url": "/Process_Lineage/static/css/main.4c84a0ec.chunk.css"
+  },
+  {
+    "revision": "5c7a8add3fbec3be4262",
+    "url": "/Process_Lineage/static/js/2.9773ef5f.chunk.js"
+  },
+  {
+    "revision": "672aa44fbf4ef81237e08dd6e8261683",
+    "url": "/Process_Lineage/static/js/2.9773ef5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "852436e734a80e0a887f",
+    "url": "/Process_Lineage/static/js/main.bea20197.chunk.js"
+  },
+  {
+    "revision": "323efbdd60d071e27856",
+    "url": "/Process_Lineage/static/js/runtime-main.121a51b2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Process_Lineage/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Process_Lineage/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Process_Lineage/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Process_Lineage/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Process_Lineage/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
